f231525167d38866d845b2c54f6b4234e1960d14
Masaya Okawa <masamasa9841@gmail.com>
2017-12-27 15:25:37 +0900

    パーツわけをした

---
+++ docs/html-parts.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#navigation").load("template-parts/navigation.html");
+    $("#header").load("template-parts/header.html");
+    $("#sidebar").load("template-parts/sidebar.html");
+    $("#footer").load("template-parts/footer.html");
+    $("#head").load("template-parts/head.html");
+});
